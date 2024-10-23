8369861cd2e72ff3ae0c57972c415f6b1343213f
Estelancs <99928545+Estelancs@users.noreply.github.com>
2024-10-23 08:59:59 -0300

    Create quantidadeUsuarios.js

---
+++ quantidadeUsuarios.js
@@ -0,0 +1,68 @@
+const getCSS = (variavel) => {
+    const bodyStyles = getComputedStyle(document.body)
+    return bodyStyles.getPropertyValue(variavel)
+}
+
+const tickConfig = {
+    family: getCSS('--font'),
+    size: 16,
+    color: getCSS('--Azul-escuro')
+}
+
+async function quantidadeUsuariosPorRede() {
+    const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
+    const res = await fetch(url)
+    const dados = await res.json()
+    const nomeDasRedes = Object.keys(dados)
+    const quantidadeDeUsuarios = Object.values(dados)
+
+    const data = [
+        {
+            x: nomeDasRedes, 
+            y: quantidadeDeUsuarios, 
+            type: 'bar',
+            marker: {
+                color: getCSS('--Azul-escuro')
+            }
+        }
+    ]
+
+    const laytout = {
+        plot_bgcolor: getCSS('--Azul-marinho'),
+        paper_bgcolor: getCSS('--Azul-marinho'),
+        title: {
+            text: 'Redes sociais com mais usuários',
+            x: 0,
+            font: {
+                color: getCSS('--Preto'),
+                size: 30,
+                font: getCSS('--font')
+            }
+        },
+        xaxis: {
+            tickfont: tickConfig,
+            title: {
+                text: 'Nome das redes',
+                font: {
+                    color: getCSS('--Preto')
+                }
+            }
+        },
+        yaxis: {
+            tickfont: tickConfig,
+            title: {
+                text: 'Bilhões de usuários ativos',
+                font: {
+                    color: getCSS('--Preto')
+                }
+            }
+        }
+    }
+
+    const grafico = document.createElement('div')
+    grafico.className = 'grafico'
+    document.getElementById('graficos-container').appendChild(grafico)
+    Plotly.newPlot(grafico, data, laytout)
+}
+
+quantidadeUsuariosPorRede()
